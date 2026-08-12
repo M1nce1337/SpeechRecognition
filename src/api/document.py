8afad1986a3c5906b0b943d4912309126dbb878a
github_username <kafanovd@bk.ru>
2026-08-12 17:15:26 +0300

    операция генерации документа вынесена в пул процессов вместо потоков

--- src/api/document.py
+++ src/api/document.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from fastapi.responses import StreamingResponse
 from core.services.document_service import document_service
+from concurrent.futures import ProcessPoolExecutor, Future
 import asyncio
 
 router = APIRouter()
@@ -8,6 +9,8 @@ router = APIRouter()
 @router.post("/document/download")
 async def download_document(request: dict) -> StreamingResponse:
     llm_response = request.get("result", {})
-    document = await asyncio.to_thread(document_service.create_document, llm_response)
+
+    with ProcessPoolExecutor() as executor:
+        document = await asyncio.get_event_loop().run_in_executor(executor, document_service.create_document, llm_response)
 
     return document     
